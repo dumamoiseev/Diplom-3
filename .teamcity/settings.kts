97c671140ebb7eea4e70a607a338e0bf98bbb3d0
duma_electronik <duma_electronik@TeamCity>
2023-08-28 07:01:38 +0000

    TeamCity change in 'Project 1' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,71 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.projectFeatures.githubAppConnection
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2023.05"
+
+project {
+
+    buildType(Build)
+
+    features {
+        githubAppConnection {
+            id = "PROJECT_EXT_70"
+            displayName = "GitHub App (2)"
+            appId = "123"
+            clientId = "123"
+            clientSecret = "credentialsJSON:8fca7e30-35fd-4f3c-ab9b-2def2ee292bf"
+            privateKey = "credentialsJSON:1a0cc515-c07d-4521-9584-258538aae002"
+            ownerUrl = "https://github.com/username"
+        }
+    }
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
